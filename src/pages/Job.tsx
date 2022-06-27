25119076249c6ca56dd249146b0e924600489d66
심은광 <sim-eungwang@sim-eungwang-ui-MacBookAir.local>
2022-06-28 01:22:41 +0900

    fetch : 각 페이지에 Header Component 추가

--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppLayout from "../components/common/AppLayout";
+import Header from "../components/common/header/Header";
 import Nav from "../components/common/header/Nav";
 
 const Job = () => {
   return (
     <AppLayout>
+      <Header />
       <Nav />
     </AppLayout>
   );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import AppLayout from "../components/common/AppLayout";
+import CustomDropdown from "../components/common/CustomDropdown";
+import Header from "../components/common/header/Header";
 import Nav from "../components/common/header/Nav";
+import Category from "../components/main/Category";
 import Hotcontent from "../components/main/Hotcontent";
 import Notefolio from "../components/main/Notefolio";
 
 const Main = () => {
   return (
     <AppLayout>
+      <Header />
       <Nav />
       <Hotcontent />
+      <Category />
       <Notefolio />
     </AppLayout>
   );
