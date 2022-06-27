8ae6b18bd200e8df85ca6e86cf693217c0a9a3a7
심은광 <sim-eungwang@sim-eungwang-ui-MacBookAir.local>
2022-06-27 18:09:18 +0900

    fetch : Search Component

---
+++ src/components/common/header/Search.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const Search = () => {
+  return (
+    <SearchCotainer>
+      <input placeholder="크리에이티브 검색" type="text" />
+      <span className="material-symbols-outlined">search</span>
+    </SearchCotainer>
+  );
+};
+
+export default Search;
+
+const SearchCotainer = styled.div`
+  max-width: 280px;
+  width: 100%;
+  height: 33px;
+  display: flex;
+  position: relative;
+  .material-symbols-outlined {
+    position: absolute;
+    fill: #444444;
+    top: 8px;
+    left: 12px;
+    font-size: 18px;
+  }
+  input {
+    width: 100%;
+    border: solid 1px #e4e8e8;
+    border-radius: 19px;
+    background-color: #f7f9f9;
+    padding: 8px 40px;
+  }
+`;
