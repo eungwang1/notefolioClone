432d0b45627e93d00dbc49d874835e2a52e2c72d
심은광 <sim-eungwang@sim-eungwang-ui-MacBookAir.local>
2022-06-28 01:21:51 +0900

    fetch : Header Component

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,15 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { logoIcon02, logoIcon03 } from "../../../assets";
 import Nav from "./Nav";
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <Nav />
+      <div className="header-banner">
+        <img src="/images/banner.png" alt="banner" width="100%" height="auto" />
+      </div>
+      <div className="family-gnb">
+        <Link to="/" className="gnb-logo">
+          <img src={logoIcon02} alt="logo" width="100%" height="auto" />
+        </Link>
+        <Link to="/" className="gnb-menu">
+          <img src={logoIcon03} alt="logo" width="100%" height="auto" />
+        </Link>
+      </div>
     </HeaderContainer>
   );
 };
 
 export default Header;
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  .gnb-logo,
+  .gnb-menu {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    padding: 10px 16px;
+  }
+  .gnb-menu {
+    background-color: #ffffff;
+    border-right: 1px solid #dfdfdf;
+    border-left: 1px solid #dfdfdf;
+    box-sizing: border-box;
+  }
+  .header-banner {
+    display: flex;
+    width: 50%;
+    margin: 0 auto;
+  }
+  .family-gnb {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-content: flex-start;
+    background-color: #f9fafc;
+    width: 100%;
+    height: 35px;
+  }
+`;
