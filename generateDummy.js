364c7c329980fc5324c5dbeecaeebf320cb53898
심은광 <sim-eungwang@sim-eungwang-ui-MacBookAir.local>
2022-06-29 15:48:34 +0900

    styled

--- generateDummy.js
+++ generateDummy.js
@@ -1,18 +1,8 @@
 let jsonFile = require("jsonfile");
 let faker = require("@faker-js/faker").faker;
-const data = {
-  data: [
-    {
-      name: "123",
-      age: 123,
-    },
-    {
-      name: "123",
-      age: 123,
-    },
-  ],
-};
-const notefolioLength = 93;
+const notefolioLength = 10000;
+const creatorListLength = 10;
+const recruitListLength = 3;
 const notefolio = Array.from({ length: notefolioLength }, (v, i) => ({
   username: faker.name.middleName(),
   profile: faker.image.avatar(),
@@ -23,17 +13,16 @@ const notefolio = Array.from({ length: notefolioLength }, (v, i) => ({
   likecount: parseInt(Math.random() * 100),
   id: i + 1 + "",
 }));
-const cardList = Array.from({ length: 8 }, (v, i) => ({
+const creatorList = Array.from({ length: creatorListLength }, (v, i) => ({
   coverImage: [faker.image.fashion(480, 480, true), faker.image.animals(480, 480, true)],
   profileImage: faker.image.avatar(),
   username: faker.lorem.word(),
   category: faker.lorem.words(2),
 }));
-const recruitList = Array.from({ length: 3 }, (v, k) => ({
+const recruitList = Array.from({ length: recruitListLength }, (v, k) => ({
   image: faker.image.city(480, 480, true),
   title: faker.lorem.words(2),
   name: faker.lorem.word(),
 }));
 
-// jsonFile.writeFile("./src/assets/db/db.json", { notefolio, cardList, recruitList });
-jsonFile.writeFile("./db.json", { notefolio, cardList, recruitList });
+jsonFile.writeFile("./db.json", { notefolio, creatorList, recruitList });
