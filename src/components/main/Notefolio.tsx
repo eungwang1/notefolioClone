8603dcc39b4259c9df9724d3f1e4ebcc0dfab3bd
eungwang <scm323@naver.com>
2022-06-30 17:33:22 +0900

    fetch : 검색, 카테고리 필터 관련작업

--- src/components/main/Notefolio.tsx
+++ src/components/main/Notefolio.tsx
@@ -1,25 +1,57 @@
-import React, { useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import styled from "styled-components";
 import useNotefolio from "../../lib/useNotefolio";
-import { useAppSelector } from "../../store/hook";
+import { useAppDispatch, useAppSelector } from "../../store/hook";
 import NotefoiloCard from "./NotefoiloCard";
 import useInfinityScroll from "../../lib/useInfinityScroll";
+import MiddleBanner from "./MiddleBanner";
+import { useMedia } from "../../lib/useMediaQuery";
+import { responsiveSliceCount } from "../../lib/responsiveValueList";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { onClearNotefolioList } from "../../slices/notefolioSlice";
 
 const Notefolio: React.FC = () => {
   const { notefolioList, getNotefolioListLoading } = useAppSelector((state) => state.notefolioSlice);
+  const [searchParms] = useSearchParams();
+  const location = useLocation();
+  const dispatch = useAppDispatch();
+  const searchValue = searchParms.get("search");
+  const category = searchParms.get("category");
   const { onLoadNotefolioList } = useNotefolio();
   const target = useRef<HTMLDivElement>(null);
+  const { isTablet, isMobile, isMobileSmall, isPcMiddle } = useMedia();
+  useEffect(() => {
+    dispatch(onClearNotefolioList());
+  }, [location.pathname, searchValue]);
+
   useInfinityScroll({
     target,
     targetArray: notefolioList,
     fetchAction: onLoadNotefolioList,
     threshold: 0,
     rootMargin: "300px 0px",
+    searchValue: searchValue as string,
+    category: category as string,
   });
+
+  const sliceCount = useMemo(() => {
+    if (isMobileSmall) return responsiveSliceCount.ms;
+    if (isMobile) return responsiveSliceCount.ml;
+    if (isTablet) return responsiveSliceCount.tb;
+    if (isPcMiddle) return responsiveSliceCount.pcs;
+    return responsiveSliceCount.pc;
+  }, [isMobileSmall, isMobile, isTablet, isPcMiddle]);
+
   return (
     <NotefolioContainer>
       <div className="notefolio-works-wrapper">
-        {notefolioList?.map((item, idx) => (
+        {notefolioList?.slice(0, sliceCount).map((item, idx) => (
+          <NotefoiloCard item={item} idx={idx} key={item.id + idx} />
+        ))}
+      </div>
+      {notefolioList.length >= 1 && <MiddleBanner />}
+      <div className="notefolio-works-wrapper">
+        {notefolioList?.slice(sliceCount).map((item, idx) => (
           <NotefoiloCard item={item} idx={idx} key={item.id + idx} />
         ))}
       </div>
@@ -33,13 +65,12 @@ const Notefolio: React.FC = () => {
 export default Notefolio;
 
 const NotefolioContainer = styled.div`
-  padding: 24px;
   width: 100vw;
   .notefolio-works-wrapper {
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
-    padding: 24px 0 0 0;
+    padding: 24px 18px;
   }
 `;
