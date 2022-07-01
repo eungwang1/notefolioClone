fecd121db08006f9d92a9e5bb18c56f40d630121
eungwang <scm323@naver.com>
2022-07-01 16:30:37 +0900

    style

--- src/actions/notefolioAction.ts
+++ src/actions/notefolioAction.ts
@@ -2,7 +2,10 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { getNotefolioListParams } from "../customTypes/params";
 
-const api = process.env.NODE_ENV ? "http://localhost:4000" : "https://my-cool-project-eungwang.herokuapp.com";
+const api =
+  process.env.NODE_ENV === "production"
+    ? "http://localhost:4000"
+    : "https://my-cool-project-eungwang.herokuapp.com";
 export const getNotefolioList = createAsyncThunk(
   "get/notefolioList",
   async ({ page, search, category }: getNotefolioListParams, thunkAPI) => {
