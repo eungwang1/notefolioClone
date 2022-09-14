3152e412b48a0d277304419b33f2d71b0b3d7dc0
Eungwang <86244477+eungwang1@users.noreply.github.com>
2022-09-14 14:38:10 +0900

    test

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const test = () => {
+  return (
+    <div>
+      <a href="/pdf/sample.pdf" target="_blank" rel="noreferrer">
+        <div className="modal-side-nav-download-wrapper">
+          <span className="material-symbols-outlined download not-draggable">download</span>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default test;
